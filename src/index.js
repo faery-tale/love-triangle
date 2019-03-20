a0281dd1f71bf670fdb6b244772aac1f6644e184
Ekaterina Denisenko <lira-gde-to@rambler.ru>
2019-03-20 19:59:46 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var total=0;  
+  var countTriangles=0;
+     for(var i=0;i<preferences.length;i++){ 
+      var firstOnes=preferences[i];
+      var secondOnes=preferences[firstOnes-1];
+      var thirdOnes=preferences[secondOnes-1];
+      if(thirdOnes===i+1){
+        countTriangles++;
+      }
+      if (countTriangles==3){
+        total++;
+        countTriangles=0;
+
+      }
+
+      }
+    
+  return total;
 };
